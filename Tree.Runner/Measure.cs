12253d78b45a62fa037228c507cd58f62d228ed1
scooletz <scooletz@gmail.com>
2022-11-28 19:18:43 +0100

    program made configurable

---
+++ Tree.Runner/Measure.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Tree.Tests;
+
+class Measure : IDisposable
+{
+    private readonly string _name;
+    private readonly int _count;
+    private readonly int _batchSize;
+    private readonly Stopwatch _stopwatch;
+
+    public Measure(string name, int count, int batchSize)
+    {
+        _name = name;
+        _count = count;
+        _batchSize = batchSize;
+
+        _stopwatch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        var elapsed = _stopwatch.Elapsed;
+        var throughput = (int)(_count / elapsed.TotalSeconds);
+        Console.WriteLine(
+            $"{_name} of {_count:N} items with batch of {_batchSize} took {elapsed.ToString()} giving a throughput {throughput:N} items/s");
+    }
+}
--- Tree.Runner/Program.cs
+++ Tree.Runner/Program.cs
@@ -1,22 +1,24 @@
 ﻿using System.Buffers.Binary;
-using System.Diagnostics;
+using Spectre.Console;
+using Tree.Tests;
 
-namespace Tree.Tests;
+namespace Tree.Runner;
 
 public static class Program
 {
-    private const int Count = 50_000_000;
-    private const int BatchSize = 10000;
-    
     public static void Main(String[] args)
     {
+        const int DefaultCount = 80_000_000;
+        const int BatchSize = 10000;
+
         var dir = Directory.GetCurrentDirectory();
         var dataPath = Path.Combine(dir, "db");
-        
+
         if (Directory.Exists(dataPath))
         {
             Directory.Delete(dataPath, true);
         }
+
         Directory.CreateDirectory(dataPath);
 
         var db = new PersistentDb(dataPath);
@@ -26,41 +28,39 @@ public static class Program
         var key = new byte[32];
         var value = new byte[32];
 
-        var batch = tree.Begin();
+        var count = AnsiConsole.Ask("[green]How many[/] key value pairs should be used for benchmark?",
+            DefaultCount);
+        var batchSize = AnsiConsole.Ask("What [green]batch size[/] should be used ?", BatchSize);
 
-        var write = Stopwatch.StartNew();
-        for (var i = 0; i < Count; i++)
+        using (new Measure("Writing", count, batchSize))
         {
-            BinaryPrimitives.WriteInt32LittleEndian(key, i);
-            BinaryPrimitives.WriteInt32LittleEndian(value, i);
-            
-            batch.Set(key, value);
-            
-            if (i % BatchSize == 0)
+            var batch = tree.Begin();
+            for (var i = 0; i < count; i++)
             {
-                batch.Commit();
-                batch = tree.Begin();
+                BinaryPrimitives.WriteInt32LittleEndian(key, i);
+                BinaryPrimitives.WriteInt32LittleEndian(value, i);
+
+                batch.Set(key, value);
+
+                if (i % batchSize == 0)
+                {
+                    batch.Commit();
+                    batch = tree.Begin();
+                }
+            }
+
+            batch.Commit();
+        }
+
+        using (new Measure("Reading", count, batchSize))
+        {
+            for (var i = 0; i < DefaultCount; i++)
+            {
+                BinaryPrimitives.WriteInt32LittleEndian(key, i);
+                tree.TryGet(key, out var v);
             }
         }
-        
-        batch.Commit();
-        
-        Stop(write, "Writing");
-        
-        var read = Stopwatch.StartNew();
-        for (var i = 0; i < Count; i++)
-        {
-            BinaryPrimitives.WriteInt32LittleEndian(key, i);
-            tree.TryGet(key, out var v);
-        }
-        Stop(read, "Reading");
-        
-        db.PrintStats();
-    }
 
-    private static void Stop(Stopwatch sw, string name)
-    {
-        var throughput = (int)(Count / sw.Elapsed.TotalSeconds);
-        Console.WriteLine($"{name} of {Count:N} items with batch of {BatchSize} took {sw.Elapsed.ToString()} giving a throughput {throughput:N} items/s");
+        db.PrintStats();
     }
 }
