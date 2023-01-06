36fb063d684c9fd3cb52930ae67ac07e25c7ed90
scooletz <scooletz@gmail.com>
2023-01-06 17:40:44 +0100

    measure inline

--- Paprika.Runner/Program.cs
+++ Paprika.Runner/Program.cs
@@ -1,11 +1,12 @@
-﻿using Paprika.Db;
+﻿using System.Diagnostics;
+using Paprika.Db;
 
 namespace Paprika.Runner;
 
 public static class Program
 {
     private const int Count = 160_000_000;
-    private const int LogEvery = 1_000_000;
+    private const int LogEvery = 10_000_000;
     private const long Gb = 1024 * 1024 * 1024L;
     private const long DbFileSize = 20 * Gb;
 
@@ -26,10 +27,9 @@ public static class Program
         var db = new MemoryMappedPagedDb(DbFileSize, dataPath, true);
         var tx = db.Begin();
 
-        byte[] key = new byte[32];
-
-        using (new Measure("Writing", Count))
+        MeasureThroughput(tx, tx =>
         {
+            var key = new byte[32];
             var random = new Random(seed);
 
             for (var i = 0; i < Count; i++)
@@ -46,15 +46,13 @@ public static class Program
                         used * DbFileSize / Gb, DbFileSize / Gb);
                 }
             }
-        }
+        }, "Writing", Count);
 
-        using (new Measure("Committing to disk just data", Count))
-        {
-            tx.Commit(CommitOptions.FlushDataOnly);
-        }
+        Measure(tx, tx => tx.Commit(CommitOptions.FlushDataOnly), "Committing to disk (just data, root transient");
 
-        using (new Measure("Reading", Count))
+        MeasureThroughput(db, db =>
         {
+            var key = new byte[32];
             var random = new Random(seed);
 
             tx = db.Begin();
@@ -78,6 +76,36 @@ public static class Program
                     Console.WriteLine("Read {0:N0} items", i);
                 }
             }
+        }, "Reading", Count);
+    }
+
+    private static void MeasureThroughput<TState>(TState t, Action<TState> action, string name, int count)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            action(t);
+        }
+        finally
+        {
+            var elapsed = sw.Elapsed;
+            var throughput = (int)(count / elapsed.TotalSeconds);
+            Console.WriteLine(
+                $"{name} of {count:N} items took {elapsed.ToString()} giving a throughput {throughput:N} items/s");
+        }
+    }
+
+    private static void Measure<TState>(TState t, Action<TState> action, string name)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            action(t);
+        }
+        finally
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{name} took {sw.Elapsed.ToString()}");
         }
     }
 }
