202b20e59e5cb4857b05ebb2b21175685c4c6e3f
scooletz <scooletz@gmail.com>
2023-04-22 19:30:22 +0200

    initial

---
+++ src/Paprika.Tests/FrameTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Paprika.Pages.Frames;
+
+namespace Paprika.Tests;
+
+public class FrameTests
+{
+    private const int MaxFrames = 16;
+    private const int Next = 23;
+
+    [TestCase(new byte[0], 1, TestName = "Empty Span")]
+    [TestCase(new byte[] { 13 }, 1, TestName = "Single byte")]
+    [TestCase(new byte[] { 1, 2, 3, 4, 5, 6 }, 1, TestName = "Full frame")]
+    [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7 }, 2, TestName = "2 frames")]
+    [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 2, TestName = "2 full frames")]
+    [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, 3, TestName = "3 frames")]
+    public void Write_Read(byte[] bytes, byte expectedFrameCount)
+    {
+        Span<Frame> pool = stackalloc Frame[MaxFrames];
+        ref var frame = ref pool[0];
+
+        Frame.GetFrameCount(bytes).Should().Be(expectedFrameCount);
+
+        Frame.Write(ref frame, bytes, Next);
+        var read = Frame.Read(ref frame, out var next);
+
+        Assert.AreEqual(Next, next);
+
+        read.StartsWith(bytes).Should()
+            .BeTrue("There can be more bytes in the frame, but they should start with original");
+    }
+}
---
+++ src/Paprika/Pages/Frames/Frame.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Paprika.Pages.Frames;
+
+/// <summary>
+/// Provides a wrapping for a <see cref="byte"/> based index of a <see cref="IFrame"/> on the page,
+/// so that 0 can be used as a value and is different from the null value. 
+/// </summary>
+[StructLayout(LayoutKind.Explicit, Size = Size)]
+public readonly struct Frame
+{
+    public const int Size = 8;
+
+    public static Span<byte> Read(ref Frame frame, out byte next)
+    {
+        var header = Unsafe.As<Frame, Header>(ref frame);
+        next = header.Next;
+
+        return CreateByteSpan(ref frame, header);
+    }
+
+    public static void Write(ref Frame frame, Span<byte> bytes, byte next)
+    {
+        var length = GetFrameCount(bytes);
+
+        ref var header = ref Unsafe.As<Frame, Header>(ref frame);
+
+        header.Length = length;
+        header.Next = next;
+
+        var dest = CreateByteSpan(ref frame, header);
+        bytes.CopyTo(dest);
+    }
+
+    private static Span<byte> CreateByteSpan(ref Frame frame, Header header)
+    {
+        ref var bytes = ref Unsafe.Add(ref Unsafe.As<Frame, byte>(ref frame), Header.Size);
+        return MemoryMarshal.CreateSpan(ref bytes, header.Length * Size - Header.Size);
+    }
+
+    /// <summary>
+    /// Gets the number of frames needed to store <see cref="bytes"/> payload.
+    /// </summary>
+    public static byte GetFrameCount(Span<byte> bytes) => (byte)AlignToFrames(bytes.Length + Header.Size);
+
+    private static int AlignToFrames(int length) => ((length + (Size - 1)) & -Size) / Size;
+
+    [StructLayout(LayoutKind.Explicit, Size = Size)]
+    private struct Header
+    {
+        // ReSharper disable once MemberHidesStaticFromOuterClass
+        public const int Size = 2;
+
+        [FieldOffset(0)] public byte Length;
+
+        [FieldOffset(1)] public byte Next;
+    }
+}
