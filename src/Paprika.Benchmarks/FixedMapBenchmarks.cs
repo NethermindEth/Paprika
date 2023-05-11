ba0de0850793d656d61243e5edb89354921922da
Szymon Kulec <scooletz@gmail.com>
2023-05-11 16:21:33 +0200

    Fixed map better benchmark (#80)
    * nibble path cleaned a bit
    
    * better benchmark
    
    * even better benchmarks
    
    * better benchmark
    
    * FixedMap benchmark and cleanup

--- src/Paprika.Benchmarks/FixedMapBenchmarks.cs
+++ src/Paprika.Benchmarks/FixedMapBenchmarks.cs
@@ -1,19 +1,19 @@
 ﻿using System.Buffers.Binary;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Disassemblers;
 using Paprika.Data;
 using Paprika.Db;
-
 namespace Paprika.Benchmarks;
 
+[DisassemblyDiagnoser(3)]
 public class FixedMapBenchmarks
 {
-    private readonly byte[] _data = new byte[Page.PageSize];
+    private readonly byte[] _writtenData = new byte[Page.PageSize];
+    private readonly byte[] _writable = new byte[Page.PageSize];
     private readonly int _to;
 
     public FixedMapBenchmarks()
     {
-        var map = new FixedMap(_data);
+        var map = new FixedMap(_writtenData);
 
         Span<byte> key = stackalloc byte[4];
 
@@ -33,20 +33,71 @@ public class FixedMapBenchmarks
     }
 
     [Benchmark]
-    public int Read_all_keys()
+    public int Write_whole_page_of_data()
     {
-        var map = new FixedMap(_data);
+        _writable.AsSpan().Clear();
+        var map = new FixedMap(_writable);
+
         Span<byte> key = stackalloc byte[4];
+
+        int count = 0;
+
+        // fill 
+        for (int i = 0; i < _to; i++)
+        {
+            BinaryPrimitives.WriteInt32LittleEndian(key, i);
+            var path = NibblePath.FromKey(key);
+            if (map.TrySet(FixedMap.Key.Account(path), key))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    [Benchmark]
+    public int Read_existing_keys()
+    {
+        var map = new FixedMap(_writtenData);
+        Span<byte> key = stackalloc byte[4];
+
+        var result = 0;
+
+        // find all values
         for (var i = 0; i < _to; i++)
         {
             BinaryPrimitives.WriteInt32LittleEndian(key, i);
             var path = NibblePath.FromKey(key);
-            if (map.TryGet(FixedMap.Key.Account(path), out var data) == false)
+            if (map.TryGet(FixedMap.Key.Account(path), out var data))
             {
-                return i;
+                result += data.Length;
             }
         }
 
-        return _to;
+        return result;
     }
+
+    [Benchmark]
+    public int Read_nonexistent_keys()
+    {
+        var map = new FixedMap(_writtenData);
+        Span<byte> key = stackalloc byte[4];
+
+        var result = 0;
+
+        // miss all the next
+        for (int i = _to; i < _to * 2; i++)
+        {
+            BinaryPrimitives.WriteInt32LittleEndian(key, i);
+            var path = NibblePath.FromKey(key);
+            if (map.TryGet(FixedMap.Key.Account(path), out _) == false)
+            {
+                result += 1;
+            }
+        }
+
+        return result;
+    }
+
 }
