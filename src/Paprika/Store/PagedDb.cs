afb7c00ae42884b01f67aaef48bfa3882d19009d
scooletz <scooletz@gmail.com>
2023-05-17 13:23:35 +0200

    in-page buffer

--- src/Paprika/Store/PagedDb.cs
+++ src/Paprika/Store/PagedDb.cs
@@ -262,7 +262,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
             return TryGetPage(key, out var page) ? page.GetStorage(GetPath(key), address, this) : default;
         }
 
-        private bool TryGetPage(Keccak key, out FanOut256Page page)
+        private bool TryGetPage(Keccak key, out DataPage page)
         {
             if (_disposed)
                 throw new ObjectDisposedException("The readonly batch has already been disposed");
@@ -274,7 +274,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
                 return false;
             }
 
-            page = new FanOut256Page(GetAt(addr));
+            page = new DataPage(GetAt(addr));
             return true;
         }
 
@@ -329,7 +329,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
         public Account GetAccount(in Keccak key) =>
             TryGetPageNoAlloc(key, out var page) ? page.GetAccount(GetPath(key), this) : default;
 
-        private bool TryGetPageNoAlloc(in Keccak key, out FanOut256Page page)
+        private bool TryGetPageNoAlloc(in Keccak key, out DataPage page)
         {
             CheckDisposed();
 
@@ -341,7 +341,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
                 return false;
             }
 
-            page = new FanOut256Page(_db.GetAt(addr));
+            page = new DataPage(_db.GetAt(addr));
             return true;
         }
 
@@ -362,7 +362,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
             addr = _db.GetAddress(updated);
         }
 
-        private ref DbAddress TryGetPageAlloc(in Keccak key, out FanOut256Page page)
+        private ref DbAddress TryGetPageAlloc(in Keccak key, out DataPage page)
         {
             CheckDisposed();
 
@@ -380,7 +380,7 @@ public class PagedDb : IPageResolver, IDb, IDisposable
                 p = GetAt(addr);
             }
 
-            page = new FanOut256Page(p);
+            page = new DataPage(p);
 
             return ref addr;
         }
