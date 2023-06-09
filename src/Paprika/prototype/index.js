f347e150df36755c05501f7f8bf1ef8e5c98528e
Lautaro Emanuel <emlautarom1@gmail.com>
2023-06-09 14:14:23 -0300

    Visualization tool

---
+++ src/Paprika/prototype/index.js
@@ -0,0 +1,225 @@
+const root = document.getElementById('root')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const NODE_WIDTH = 200
+const NODE_HEIGHT = 50
+const PADDING = 10
+const LINE_WIDTH = 2
+
+const DELAY = 500
+
+function delay() {
+    return new Promise((res, _) => {
+        setTimeout(() => { res() }, DELAY);
+    })
+}
+
+function substrings(str) {
+    let res = []
+    for (let i = 0; i <= str.length; i++) {
+        res.push(str.substring(0, i));
+    }
+    return res;
+}
+
+class Db {
+    constructor(ctx) {
+        this._ctx = ctx
+        this._ptrs = {}
+    }
+
+    setNodes(nodes) {
+        this._ptrs = nodes
+    }
+
+    async update(path, value) {
+        let node = this.getNode(path)
+        if (!node) { throw new Error(`No associated node for path ${path}`) }
+
+        node._value = value
+        node._dirty = true
+        for (let prefix of substrings(path)) {
+            let node = this.getNode(prefix)
+            if (node) {
+                node._dirty = true
+                this.displayNodes()
+            }
+        }
+    }
+
+    // We compute the root hash starting on the empty prefix
+    async rootHash() {
+        let rootNode = this._ptrs['']
+        if (rootNode) {
+            return await rootNode.hash('', this)
+        } else {
+            return "<unknown>"
+        }
+    }
+
+    getNode(path) { return this._ptrs[path] }
+
+    displayNodes() {
+        this._ctx.clearRect(0, 0, 500, 500)
+
+        let kvs = Object.entries(this._ptrs);
+        kvs.sort(([k1, _e1], [k2, _e2]) => k1.length - k2.length);
+
+        let offsets = { x: -1, y: 0 }
+        for (let [path, node] of kvs) {
+            let depth = path.length
+
+            if (offsets.y < depth) {
+                offsets.x = 0;
+                offsets.y = depth
+            } else {
+                offsets.x++;
+            }
+
+            let x = PADDING + offsets.x * (NODE_WIDTH + PADDING)
+            let y = PADDING + (offsets.y * (NODE_HEIGHT + PADDING))
+
+            this._ctx.fillStyle = node.color()
+            this._ctx.lineWidth = LINE_WIDTH
+            this._ctx.fillRect(x, y, NODE_WIDTH, NODE_HEIGHT)
+
+            if (node._dirty) {
+                this._ctx.strokeStyle = 'red';
+                this._ctx.strokeRect(x, y, NODE_WIDTH, NODE_HEIGHT)
+            }
+
+            this._ctx.font = `${NODE_HEIGHT / 3}px Arial`
+            this._ctx.fillStyle = 'black'
+            let text = `/${path.split('').join('/')} : ${node.toString()}`
+            this._ctx.fillText(text, PADDING + x, (NODE_HEIGHT + PADDING) / 2 + y)
+        }
+    }
+}
+
+class Node {
+    constructor(value, onHash) {
+        this._value = value
+        this._dirty = true
+        this._hash = undefined
+        this._onHash = onHash
+    }
+
+    async computeHash(path, db) { throw new Error('not implemented') }
+
+    async hash(path, db) {
+        if (!this._dirty) { return this._hash }
+
+        this._hash = await this.computeHash(path, db)
+
+        this._dirty = false
+        await this._onHash()
+        return this._hash
+    }
+}
+
+class Leaf extends Node {
+    constructor(value, onHash) {
+        super(value, onHash)
+    }
+
+    toString() { return `"${this._value}"` }
+
+    color() { return 'rgb(200, 190, 220)' }
+
+    async computeHash(path, db) {
+        return `h(${path} -> ${this._value})`
+    }
+}
+
+class Extension extends Node {
+    constructor(value, onHash) {
+        super(value, onHash)
+    }
+
+    toString() { return `<${this._value}>` }
+
+    color() { return 'rgb(215, 230, 190)' }
+
+    async computeHash(path, db) {
+        let childPath = path + this._value
+        let child = db.getNode(childPath)
+        let childHash = await child.hash(childPath, db)
+        return `h(${childHash})`
+    }
+}
+
+class Branch extends Node {
+    constructor(value, onHash) {
+        super(value, onHash)
+    }
+
+    toString() { return `[${this._value}]` }
+
+    color() { return 'rgb(180, 220, 240)' }
+
+    async computeHash(path, db) {
+        let childrenHashes = []
+        for (let i = 0; i < this._value.length; i++) {
+            let nibble = this._value[i]
+
+            let childPath = path + nibble
+            let child = db.getNode(childPath)
+
+            let childHash = await child.hash(childPath, db)
+            childrenHashes.push(childHash)
+        }
+        return `h(${childrenHashes.join(' + ')})`
+    }
+}
+
+function mkNode({ type, value }, onHash) {
+    switch (type) {
+        case 'leaf': return new Leaf(value, onHash)
+        case 'branch': return new Branch(value, onHash)
+        case 'extension': return new Extension(value, onHash)
+    }
+}
+
+function displayRoot(value) {
+    root.innerText = value
+}
+
+async function main() {
+    /*
+    Storage is:
+    ABC -> "Hello"
+    ABD -> "World"
+
+    Full tree structure:
+    0 -> Extension; AB
+    AB -> Branch; [C, D]
+    ABC -> Leaf; "Hello"
+    ABD -> Leaf; "World"
+    */
+    const db = new Db(ctx)
+    const onHash = async () => {
+        await delay()
+        db.displayNodes()
+    }
+    db.setNodes({
+        '': mkNode({ type: 'extension', value: 'AB' }, onHash),
+        'AB': mkNode({ type: 'branch', value: ['C', 'D'] }, onHash),
+        'ABC': mkNode({ type: 'leaf', value: 'Hello' }, onHash),
+        'ABD': mkNode({ type: 'leaf', value: 'World' }, onHash),
+    })
+    db.displayNodes()
+    displayRoot('<unknown>')
+
+    displayRoot(await db.rootHash())
+
+    await delay()
+    await db.update('ABC', 'updated1')
+    displayRoot(await db.rootHash())
+
+    await delay()
+    await db.update('ABD', 'updated2')
+    displayRoot(await db.rootHash())
+}
+
+main()
