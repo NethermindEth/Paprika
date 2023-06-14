44532c7c656bf0271a118f3d652dd2ba0ce47ba4
scooletz <scooletz@gmail.com>
2023-06-14 10:27:05 +0200

    one more test

--- src/Paprika.Tests/HashingMapTests.cs
+++ src/Paprika.Tests/HashingMapTests.cs
@@ -1,31 +1,97 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using Paprika.Crypto;
 using Paprika.Data;
 
 namespace Paprika.Tests;
 
 public class HashingMapTests
 {
-    private static NibblePath Key0 => NibblePath.FromKey(Values.Key0);
+    private static NibblePath Path0 => NibblePath.FromKey(Values.Key0);
+    private static NibblePath Path1 => NibblePath.FromKey(Values.Key1a);
     private static ReadOnlySpan<byte> Data0 => new byte[] { 23 };
+    private static ReadOnlySpan<byte> Data1 => new byte[] { 37 };
 
     [Test]
-    public void Set_Get()
+    public void Set_Get_Should_use_hash_for_operations()
     {
         Span<byte> span = stackalloc byte[HashingMap.MinSize];
         var map = new HashingMap(span);
 
-        var key = Key.Account(Key0);
+        var key = Key.Account(Path0);
 
         var hash = HashingMap.GetHash(key);
         var data = Data0;
 
-        hash.Should().NotBe(HashingMap.NoCache);
+        hash.Should().NotBe(HashingMap.Null);
 
         map.SetAssert(hash, key, data);
         map.GetAssert(hash, key, data);
 
-        map.TryGet(hash + 1, key, out _).Should().BeFalse();
+        var differentHash = hash + 1;
+        map.TryGet(differentHash, key, out _).Should().BeFalse();
+    }
+
+    [Test]
+    public void On_full_should_report_false()
+    {
+        Span<byte> span = stackalloc byte[HashingMap.MinSize];
+        var map = new HashingMap(span);
+
+        var key0 = Key.Account(Path0);
+        var hash0 = HashingMap.GetHash(key0);
+
+        var key1 = Key.Account(Path0);
+        var hash1 = HashingMap.GetHash(key1);
+
+        var data = Data0;
+
+        hash0.Should().NotBe(HashingMap.Null);
+
+        map.SetAssert(hash0, key0, data);
+        map.TrySet(hash1, key1, data).Should().BeFalse();
+
+        map.GetAssert(hash0, key0, data);
+    }
+
+    [Test]
+    public void Colliding_hashes()
+    {
+        Span<byte> span = stackalloc byte[HashingMap.MinSize * 2];
+        var map = new HashingMap(span);
+
+        uint hash = 257;
+        var key0 = Key.Account(Path0);
+        var key1 = Key.Account(Path1);
+
+        var data0 = Data0;
+        var data1 = Data1;
+
+        map.SetAssert(hash, key0, data0);
+        map.SetAssert(hash, key1, data1);
+
+        map.GetAssert(hash, key0, data0);
+        map.GetAssert(hash, key1, data1);
+    }
+
+    [Test]
+    public void Clear()
+    {
+        Span<byte> span = stackalloc byte[HashingMap.MinSize];
+        var map = new HashingMap(span);
+
+        var key = Key.Account(Path0);
+
+        var hash = HashingMap.GetHash(key);
+        var data = Data0;
+
+        hash.Should().NotBe(HashingMap.Null);
+
+        map.SetAssert(hash, key, data);
+
+        map.Clear();
+
+        map.TryGet(hash, key, out _).Should().BeFalse();
     }
 }
 
@@ -39,6 +105,6 @@ static class TestExtensions
     public static void GetAssert(this HashingMap map, uint hash, in Key key, ReadOnlySpan<byte> expected)
     {
         map.TryGet(hash, key, out var actual).Should().BeTrue();
-        actual.SequenceEqual(expected).Should().BeTrue("Actual data should equal expected");
+        actual.SequenceEqual(expected).Should().BeTrue($"Actual data {actual.ToHexString(false)} should equal expected {expected.ToHexString(false)}");
     }
 }
--- src/Paprika/Data/HashingMap.cs
+++ src/Paprika/Data/HashingMap.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Hashing;
+﻿using System.Buffers;
+using System.IO.Hashing;
 using System.Runtime.InteropServices;
 using Paprika.Crypto;
 
@@ -22,7 +23,7 @@ public readonly ref struct HashingMap
     private const int SkipInitialNibbles = 6;
 
     private const int MinimumPathLength = Keccak.Size * NibblePath.NibblePerByte - SkipInitialNibbles;
-    public const uint NoCache = 0;
+    public const uint Null = 0;
     private const uint HasCache = 0b1000_0000_0000_0000;
 
     private const int TypeBytes = 1;
@@ -49,15 +50,20 @@ public readonly ref struct HashingMap
 
     public bool TryGet(uint hash, in Key key, out ReadOnlySpan<byte> value)
     {
-        int position;
-        while ((position = _hashes.IndexOf(hash)) != IndexOfNotFound)
+        var hashes = _hashes;
+
+        var offset = 0;
+        int index;
+
+        while ((index = hashes.IndexOf(hash)) != IndexOfNotFound)
         {
-            var entry = GetEntry(position);
-            var path = key.Path;
+            offset += index;
+
+            var entry = GetEntry(offset);
 
             // ReSharper disable once StackAllocInsideLoop, highly unlikely as this should be only one hit
-            Span<byte> destination = stackalloc byte[path.MaxByteLength + TypeBytes + key.AdditionalKey.Length];
-            var prefix = GeneratePrefix(key, path, destination);
+            Span<byte> destination = stackalloc byte[key.Path.MaxByteLength + TypeBytes + LengthOfLength + key.AdditionalKey.Length];
+            var prefix = WritePrefix(key, destination);
 
             if (entry.StartsWith(prefix))
             {
@@ -65,6 +71,15 @@ public readonly ref struct HashingMap
                 value = entry.Slice(prefix.Length + LengthOfLength, dataLength);
                 return true;
             }
+
+            if (index + 1 >= hashes.Length)
+            {
+                // the span is empty and there's not place to move forward
+                break;
+            }
+
+            hashes = hashes.Slice(index + 1);
+            offset += 1;
         }
 
         value = default;
@@ -76,7 +91,7 @@ public readonly ref struct HashingMap
     public bool TrySet(uint hash, in Key key, ReadOnlySpan<byte> value)
     {
         // no collision detection, just append at the first empty
-        var position = _hashes.IndexOf(NoCache);
+        var position = _hashes.IndexOf(Null);
         if (position == IndexOfNotFound)
         {
             return false;
@@ -84,9 +99,8 @@ public readonly ref struct HashingMap
 
         _hashes[position] = hash;
 
-        var path = key.Path;
-        Span<byte> destination = stackalloc byte[path.MaxByteLength + TypeBytes + key.AdditionalKey.Length];
-        var prefix = GeneratePrefix(key, path, destination);
+        Span<byte> destination = stackalloc byte[GetPrefixAllocationSize(key)];
+        var prefix = WritePrefix(key, destination);
 
         // get the entry
         var entry = GetEntry(position);
@@ -99,34 +113,176 @@ public readonly ref struct HashingMap
         return true;
     }
 
+    private static int GetPrefixAllocationSize(in Key key) =>
+        key.Path.MaxByteLength + // path 
+        TypeBytes +  // type
+        LengthOfLength + key.AdditionalKey.Length; // additional key
+
     public static uint GetHash(in Key key)
     {
         if (CanBeCached(key))
         {
-            var path = TrimPath(key);
+            var k = TrimToRightPathLenght(key);
 
-            Span<byte> destination = stackalloc byte[path.MaxByteLength + TypeBytes + key.AdditionalKey.Length];
+            Span<byte> destination = stackalloc byte[GetPrefixAllocationSize(key)];
 
-            var prefix = GeneratePrefix(key, path, destination);
+            var prefix = WritePrefix(k, destination);
 
             return XxHash32.HashToUInt32(prefix) | HasCache;
         }
 
-        return NoCache;
+        return Null;
     }
 
-    private static Span<byte> GeneratePrefix(in Key key, NibblePath sliced, Span<byte> destination)
+    public void Clear()
     {
-        var leftover = sliced.WriteToWithLeftover(destination);
+        _hashes.Clear();
+
+        // it's sufficient to clear the caches
+        //_entries.Clear();
+    }
+
+    private static Span<byte> WritePrefix(in Key key, Span<byte> destination)
+    {
+        var leftover = key.Path.WriteToWithLeftover(destination);
         leftover[0] = (byte)key.Type;
-        key.AdditionalKey.CopyTo(leftover.Slice(TypeBytes));
+        leftover[TypeBytes] = (byte)key.AdditionalKey.Length;
+
+        key.AdditionalKey.CopyTo(leftover.Slice(TypeBytes + LengthOfLength));
 
         var written = destination.Slice(0,
-            destination.Length - leftover.Length + TypeBytes + key.AdditionalKey.Length);
+            destination.Length - leftover.Length + TypeBytes + LengthOfLength + key.AdditionalKey.Length);
         return written;
     }
+    //
+    // private static Enumerator.Item ParseItem(ReadOnlySpan<byte> entry)
+    // {
+    //     var leftover = NibblePath.ReadFrom(entry, out var path);
+    //     var type = (DataType)leftover[0];
+    //
+    //     var leftover = sliced.WriteToWithLeftover(destination);
+    //     leftover[0] = (byte)key.Type;
+    //     key.AdditionalKey.CopyTo(leftover.Slice(TypeBytes));
+    //
+    //     var written = destination.Slice(0,
+    //         destination.Length - leftover.Length + TypeBytes + key.AdditionalKey.Length);
+    //     return written;
+    // }
+    //
+    // public Enumerator GetEnumerator() => new(this);
+    //
+    // public ref struct Enumerator
+    // {
+    //     /// <summary>The map being enumerated.</summary>
+    //     private readonly HashingMap _map;
+    //
+    //     private readonly int _count;
+    //
+    //     /// <summary>The next index to yield.</summary>
+    //     private int _index;
+    //
+    //     internal Enumerator(HashingMap map)
+    //     {
+    //         _map = map;
+    //         _count = map._hashes.IndexOf(Null);
+    //     }
+    //
+    //     /// <summary>Advances the enumerator to the next element of the span.</summary>
+    //     public bool MoveNext()
+    //     {
+    //         int index = _index + 1;
+    //         if (index < _count)
+    //         {
+    //             _index = index;
+    //             return true;
+    //         }
+    //
+    //         return false;
+    //     }
+    //
+    //     public Item Current
+    //     {
+    //         get
+    //         {
+    //             var hash = _map._hashes[_index];
+    //             var entry = _map.GetEntry(_index);
+    //
+    //             ref var slot = ref _map._slots[_index];
+    //             var span = _map.GetSlotPayload(ref slot);
+    //
+    //             ReadOnlySpan<byte> data;
+    //             NibblePath path;
+    //
+    //             // path rebuilding
+    //             Span<byte> nibbles = stackalloc byte[3];
+    //             var count = slot.DecodeNibblesFromPrefix(nibbles);
+    //
+    //             if (count == 0)
+    //             {
+    //                 // no nibbles stored in the slot, read as is.
+    //                 data = NibblePath.ReadFrom(span, out path);
+    //             }
+    //             else
+    //             {
+    //                 // there's at least one nibble extracted
+    //                 var raw = NibblePath.RawExtract(span);
+    //                 data = span.Slice(raw.Length);
+    //
+    //                 const int space = 2;
+    //
+    //                 var bytes = _bytes.AsSpan(0, raw.Length + space); //big enough to handle all cases
+    //
+    //                 // copy forward enough to allow negative pointer arithmetics
+    //                 var pathDestination = bytes.Slice(space);
+    //                 raw.CopyTo(pathDestination);
+    //
+    //                 // Terribly unsafe region!
+    //                 // Operate on the copy, pathDestination, as it will be overwritten with unsafe ref.
+    //                 NibblePath.ReadFrom(pathDestination, out path);
+    //
+    //                 var countOdd = (byte)(count & 1);
+    //                 for (var i = 0; i < count; i++)
+    //                 {
+    //                     path.UnsafeSetAt(i - count - 1, countOdd, nibbles[i]);
+    //                 }
+    //
+    //                 path = path.CopyWithUnsafePointerMoveBack(count);
+    //             }
+    //
+    //             if (slot.Type == DataType.StorageCell)
+    //             {
+    //                 const int size = Keccak.Size;
+    //                 var additionalKey = data.Slice(0, size);
+    //                 return new Item(Key.StorageCell(path, additionalKey), data.Slice(size), slot.Type);
+    //             }
+    //
+    //             return new Item(Key.Raw(path, slot.Type), data, slot.Type);
+    //         }
+    //     }
+    //
+    //     public void Dispose()
+    //     {
+    //         if (_bytes != null)
+    //             ArrayPool<byte>.Shared.Return(_bytes);
+    //     }
+    //
+    //     public readonly ref struct Item
+    //     {
+    //         public uint Hash { get; }
+    //         public DataType Type { get; }
+    //         public Key Key { get; }
+    //         public ReadOnlySpan<byte> RawData { get; }
+    //
+    //         public Item(Key key, ReadOnlySpan<byte> rawData, DataType type)
+    //         {
+    //             Type = type;
+    //             Key = key;
+    //             RawData = rawData;
+    //         }
+    //     }
+    // }
 
-    private static NibblePath TrimPath(in Key key) => key.Path.SliceFrom(key.Path.Length - MinimumPathLength);
+    private static Key TrimToRightPathLenght(in Key key) => key.SliceFrom(key.Path.Length - MinimumPathLength);
 
     public static bool CanBeCached(in Key key) => key.Path.Length >= MinimumPathLength;
 }
