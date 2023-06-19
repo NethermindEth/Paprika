2989f9124930f8382040d9edec236cb895745e8d
Lautaro Emanuel <emlautarom1@gmail.com>
2023-06-19 10:19:26 -0300

    Comment out everything
    - Forcing the iterator causes crashes

--- src/Paprika/Merkle/RecomputeMerkle.cs
+++ src/Paprika/Merkle/RecomputeMerkle.cs
@@ -6,10 +6,10 @@ public class RecomputeMerkle : IPreCommitBehavior
 {
     public void BeforeCommit(ICommit commit)
     {
-        foreach (var key in commit)
-        {
-            // Mark each intermediate Merkle node for each key as 'Dirty'
-        }
+        // foreach (var key in commit)
+        // {
+        //      Mark each intermediate Merkle node for each key as 'Dirty'
+        // }
         // Modify the tree structure if there were any inserts (7 cases)
         // Recompute all Keccaks in intermediate nodes
     }
