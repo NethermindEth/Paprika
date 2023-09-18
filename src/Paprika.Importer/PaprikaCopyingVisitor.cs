59ebca260610930a5ecf97924c1c43cbf293f64a
scooletz <scooletz@gmail.com>
2023-09-18 13:09:26 +0200

    decoding database

---
+++ src/Paprika.Importer/PaprikaCopyingVisitor.cs
@@ -0,0 +1,21 @@
+﻿using Nethermind.Core.Crypto;
+using Nethermind.Trie;
+
+namespace Paprika.Importer;
+
+public class PaprikaCopyingVisitor : ITreeLeafVisitor
+{
+    private long accounts;
+
+    public void VisitLeafAccount(in ValueKeccak account, Nethermind.Core.Account value)
+    {
+        Interlocked.Increment(ref accounts);
+    }
+
+    public void VisitLeafStorage(in ValueKeccak account, in ValueKeccak storage, ReadOnlySpan<byte> value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Accounts => Volatile.Read(ref accounts);
+}
--- src/Paprika.Importer/Program.cs
+++ src/Paprika.Importer/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+using Nethermind.Core;
 using Nethermind.Core.Crypto;
 using Nethermind.Db;
 using Nethermind.Db.Rocks;
@@ -8,8 +10,7 @@ using Nethermind.Logging;
 using Nethermind.Serialization.Rlp;
 using Nethermind.State;
 using Nethermind.Trie.Pruning;
-
-Console.WriteLine("Hello, World!");
+using Paprika.Importer;
 
 // StandardDbInitializer
 // MemoryHintMan
@@ -19,29 +20,63 @@ const string path = @"C:\Users\Szymon\ethereum\execution\nethermind_db\sepolia";
 var logs = LimboLogs.Instance;
 var cfg = DbConfig.Default;
 
-using var state = new DbOnTheRocks(path, new RocksDbSettings(DbNames.State, DbNames.State), cfg, logs);
-using var blockInfos = new DbOnTheRocks(path, new RocksDbSettings(DbNames.BlockInfos, DbNames.BlockInfos), cfg, logs);
-using var headers = new DbOnTheRocks(path, new RocksDbSettings(DbNames.Headers, DbNames.Headers), cfg, logs);
+using var state = new DbOnTheRocks(path, GetSettings(DbNames.State), cfg, logs).WithEOACompressed();
+using var blockInfos = new DbOnTheRocks(path, GetSettings(DbNames.BlockInfos), cfg, logs);
+using var headers = new DbOnTheRocks(path, GetSettings(DbNames.Headers), cfg, logs);
 using var store = new TrieStore(state, logs);
 
-var headAddressInDb = Keccak.Zero;
-var headData = blockInfos.Get(headAddressInDb);
-ArgumentNullException.ThrowIfNull(headData, "Block info head not found");
+// from BlockTree.cs
+byte[] stateHeadHashDbEntryAddress = new byte[16];
+var persistedNumberData = blockInfos.Get(stateHeadHashDbEntryAddress);
+long? bestPersistedState = persistedNumberData is null ? null : new RlpStream(persistedNumberData).DecodeLong();
 
-var head = new Keccak(headData);
-var headerData = headers.Get(head);
-ArgumentNullException.ThrowIfNull(headerData, "Header of the head");
+ArgumentNullException.ThrowIfNull(bestPersistedState, "Best persisted state not found");
 
-var decoder = new HeaderDecoder();
-var headHeader = decoder.Decode(new RlpStream(headerData));
-ArgumentNullException.ThrowIfNull(headHeader, "Head has no header!");
-ArgumentNullException.ThrowIfNull(headHeader.StateRoot, "Head header! has no StateRoot!");
+var chaininfo = blockInfos.Get(bestPersistedState.Value);
+var chainLevel = Rlp.GetStreamDecoder<ChainLevelInfo>()!.Decode(new RlpStream(chaininfo!));
+
+var main = chainLevel.BlockInfos[0];
+var header = headers.Get(main.BlockHash);
+
+var headerDecoded = Rlp.GetStreamDecoder<BlockHeader>()!.Decode(new RlpStream(header!));
+
+var rootHash = headerDecoded.StateRoot!;
 
 var trie = new StateTree(store, logs)
 {
-    RootHash = headHeader.StateRoot
+    RootHash = rootHash
 };
 
-trie.RootRef!.ResolveNode(store);
+var visitor = new PaprikaCopyingVisitor();
 
-Console.WriteLine($"Root: {trie.RootHash}" );
+Console.WriteLine("Starting visitor...");
+
+var sw = Stopwatch.StartNew();
+
+var run = Task.Run(() => trie.Accept(visitor, rootHash, true));
+
+while (true)
+{
+    var completed = await Task.WhenAny(Task.Delay(1_000), run);
+    if (completed == run)
+        break;
+    Console.WriteLine($"Read accounts: {visitor.Accounts}");
+}
+
+Console.WriteLine($"Root: {rootHash} had {visitor.Accounts} accounts in {sw.Elapsed:g}");
+
+return;
+
+RocksDbSettings GetSettings(string dbName)
+{
+    var dbPath = dbName;
+
+    if (dbName == DbNames.State)
+    {
+        // pruned database, append version 
+        dbPath = Path.Combine(dbName, "0");
+        dbName += "0";
+    }
+
+    return new RocksDbSettings(dbName, dbPath);
+}
