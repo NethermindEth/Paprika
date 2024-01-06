921f2f8e3248d2903ee87d888f47fffe961b0149
scooletz <scooletz@gmail.com>
2024-01-06 12:41:27 +0100

    lmdb based test

---
+++ src/Paprika/LMDB/Db.cs
@@ -0,0 +1,399 @@
+﻿using System.Buffers.Binary;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Paprika.Chain;
+using Paprika.Crypto;
+using Paprika.Data;
+using Paprika.Store;
+using Paprika.Utils;
+using Spreads.Buffers;
+using Spreads.LMDB;
+
+namespace Paprika.LMDB;
+
+public class Db : IDb, IDisposable
+{
+    private readonly int _historyDepth;
+    private readonly LMDBEnvironment _env;
+    private readonly Database _db;
+    private readonly object _batchLock = new();
+    private readonly Queue<ReadOnlyBatchCountingRefs> _readers = new();
+
+    private readonly Meter _meter;
+    private readonly MetricsExtensions.IAtomicIntGauge _depth;
+
+    /// <summary>
+    /// Used for storing metadata and mapping between accounts and their counters.
+    /// </summary>
+    /// <remarks>
+    /// 0xFF, 1 - metadata
+    /// 0xFF, 2 - account counter
+    /// 0xFF + 32bytes -> id of an account
+    /// 
+    /// </remarks>
+    private const byte MetaPrefix = 0xFF;
+
+    private const int MetaPrefixLength = 1;
+
+    /// <summary>
+    /// For compressed keys <see cref="DataType.CompressedAccount"/>, they will always start with 1
+    /// and compress much better than 64 bytes. For non compressed, this should be much smaller than that.
+    /// </summary>
+    private const int MaxMappedKeyLength = 64;
+
+    private const int MaxAccountIdLength = 8;
+    private const int AccountMapKeyLength = MetaPrefixLength + Keccak.Size;
+
+    private static ReadOnlySpan<byte> MetadataKey => new byte[] { MetaPrefix, 1 };
+    private static ReadOnlySpan<byte> AccountCounter => new byte[] { MetaPrefix, 2 };
+
+    public Db(string path, int historyDepth, long maxMapSize, bool sync = true)
+    {
+        _historyDepth = historyDepth;
+
+        const LMDBEnvironmentFlags noSyncFlags = LMDBEnvironmentFlags.NoSync | LMDBEnvironmentFlags.WriteMap;
+
+        _env = LMDBEnvironment.Create(path, sync ? LMDBEnvironmentFlags.NoMetaSync : noSyncFlags);
+        _env.MapSize = maxMapSize;
+        _env.Open();
+
+        _db = _env.OpenDatabase(null, new DatabaseConfig(DbFlags.Create));
+
+        _meter = new Meter("Paprika.Store.LMDB");
+        _depth = _meter.CreateAtomicObservableGauge("BTree depth", "Depth", "The number of levels in BTree");
+
+        PushLatestReader();
+    }
+
+    public IBatch BeginNextBatch() => Batch.ReadWrite(this, _env.BeginTransaction());
+
+    public IReadOnlyBatch BeginReadOnlyBatch(string name = "")
+    {
+        lock (_batchLock)
+        {
+            var last = _readers.Last();
+            last.AcquireLease();
+            return last;
+        }
+    }
+
+    public void Flush()
+    {
+        // TODO: what to do here?
+    }
+
+    public IReadOnlyBatch BeginReadOnlyBatchOrLatest(in Keccak stateHash, string name = "")
+    {
+        lock (_batchLock)
+        {
+            ReadOnlyBatchCountingRefs last = null!;
+            foreach (var reader in _readers)
+            {
+                last = reader;
+                if (reader.Metadata.StateHash == stateHash)
+                {
+                    break;
+                }
+            }
+
+            last.AcquireLease();
+            return last;
+        }
+    }
+
+    public bool HasState(in Keccak keccak)
+    {
+        lock (_batchLock)
+        {
+            foreach (var reader in _readers)
+            {
+                if (reader.Metadata.StateHash == keccak)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    private bool TryMapKey<TAccountContext>(in TAccountContext ctx, in Key key, Span<byte> destination,
+        out Span<byte> mapped)
+        where TAccountContext : struct, IAccountContext
+    {
+        if (key.Path.Length == NibblePath.KeccakNibbleCount)
+        {
+            // It can be Account, Storage, or Merkle for the Storage
+            // First try to get its account id.
+            if (TryGetAccountId(ctx.Tx, key.Path.UnsafeAsKeccak, out var id))
+            {
+                mapped = CompressKey(key, id, destination);
+                return true;
+            }
+
+            if (ctx.CanGenerate)
+            {
+                id = ctx.Generate(key.Path.UnsafeAsKeccak);
+                mapped = CompressKey(key, id, destination);
+                return true;
+            }
+
+            // The account that should be mapped does not exist
+            mapped = default;
+            return false;
+        }
+
+        Debug.Assert(key.Type == DataType.Merkle &&
+                     key.StoragePath.Length == 0 &&
+                     key.Path.Length < NibblePath.KeccakNibbleCount);
+
+        // directly use path as the key. Path will have set the first bit to zero,
+        // which will not collide with the encoding above
+        mapped = key.Path.WriteTo(destination);
+        return true;
+    }
+
+    private static Span<byte> CompressKey(in Key key, ReadOnlySpan<byte> id, Span<byte> destination)
+    {
+        Debug.Assert(key.Path.Length == NibblePath.KeccakNibbleCount);
+
+        var k = key.Type | DataType.CompressedAccount;
+
+        // move k up, so that the first bit is set
+        var firstByte = ((int)k << 5) + id.Length;
+
+        destination[0] = (byte)firstByte;
+        id.CopyTo(destination[1..]);
+
+        if (key.StoragePath.Length == 0)
+        {
+            return destination.Slice(0, 1 + id.Length);
+        }
+
+        var storagePathLength = key.StoragePath.WriteTo(destination[(1 + id.Length)..]).Length;
+        var totalLength = 1 + id.Length + storagePathLength;
+        return destination.Slice(0, totalLength);
+    }
+
+    [SkipLocalsInit]
+    private bool TryGetAccountId(ReadOnlyTransaction tx, in Keccak keccak, out ReadOnlySpan<byte> id)
+    {
+        Span<byte> span = stackalloc byte[AccountMapKeyLength];
+        return TryGet(tx, BuildAccountMapKey(keccak, span), out id);
+    }
+
+    private static Span<byte> BuildAccountMapKey(in Keccak keccak, Span<byte> span)
+    {
+        span[0] = MetaPrefix;
+        keccak.Span.CopyTo(span[1..]);
+        return span.Slice(0, AccountMapKeyLength);
+    }
+
+    private bool TryGet(ReadOnlyTransaction tx, scoped ReadOnlySpan<byte> key, out ReadOnlySpan<byte> value)
+    {
+        var k = ToBuffer(key);
+
+        if (_db.TryGet(tx, ref k, out var v))
+        {
+            value = v.Span;
+            return true;
+        }
+
+        value = default;
+        return false;
+    }
+
+    private void Put(Transaction tx, scoped ReadOnlySpan<byte> key, scoped ReadOnlySpan<byte> value)
+    {
+        var k = ToBuffer(key);
+        var v = ToBuffer(value);
+
+        _db.Put(tx, ref k, ref v);
+    }
+
+    private void Delete(Transaction tx, scoped ReadOnlySpan<byte> key)
+    {
+        var k = ToBuffer(key);
+        _db.Delete(tx, ref k);
+    }
+
+    private static DirectBuffer ToBuffer(ReadOnlySpan<byte> key)
+    {
+        return new DirectBuffer(MemoryMarshal.CreateSpan(ref MemoryMarshal.GetReference(key), key.Length));
+    }
+
+    public void Dispose()
+    {
+        lock (_batchLock)
+        {
+            while (_readers.TryDequeue(out var reader))
+            {
+                reader.Dispose();
+            }
+        }
+
+        _db.Dispose();
+        _env.Dispose();
+        _meter.Dispose();
+    }
+
+    class Batch : IBatch
+    {
+        private readonly Db _db;
+        private readonly Transaction _tx;
+        private readonly ReadOnlyTransaction _read;
+
+        public static Batch ReadWrite(Db db, Transaction tx) => new(db, tx, tx);
+        public static Batch ReadOnly(Db db, ReadOnlyTransaction tx) => new(db, default, tx);
+
+        private Batch(Db db, Transaction tx, ReadOnlyTransaction read)
+        {
+            _db = db;
+            _tx = tx;
+            _read = read;
+
+            Metadata = _db.TryGet(_read, MetadataKey, out var value)
+                ? Metadata.ReadFrom(value)
+                : default;
+        }
+
+        public Metadata Metadata { get; private set; }
+
+        public bool TryGet(scoped in Key key, out ReadOnlySpan<byte> result)
+        {
+            var ctx = new ReadonlyAccountContext(_read);
+            if (_db.TryMapKey(ctx, key, stackalloc byte[MaxMappedKeyLength], out var mapped))
+            {
+                return _db.TryGet(_read, mapped, out result);
+            }
+
+            result = default;
+            return false;
+        }
+
+        public void SetMetadata(uint blockNumber, in Keccak blockHash)
+        {
+            Span<byte> destination = stackalloc byte[Metadata.Size];
+
+            Metadata = new Metadata(blockNumber, blockHash);
+            Metadata.WriteTo(destination);
+
+            _db.Put(_tx, MetadataKey, destination);
+        }
+
+        public void SetRaw(in Key key, ReadOnlySpan<byte> rawData)
+        {
+            var ctx = new ReadWriteAccountContext(_tx, _db);
+            if (!_db.TryMapKey(ctx, key, stackalloc byte[MaxMappedKeyLength], out var mapped))
+            {
+                throw new Exception("Should id map properly");
+            }
+
+            _db.Put(_tx, mapped, rawData);
+        }
+
+        [SkipLocalsInit]
+        public void Destroy(in NibblePath account)
+        {
+            Debug.Assert(account.Length == NibblePath.KeccakNibbleCount);
+
+            // TODO: apply proper delete, otherwise db will grow
+            _db.Delete(_tx, BuildAccountMapKey(account.UnsafeAsKeccak, stackalloc byte[AccountMapKeyLength]));
+        }
+
+        public ValueTask Commit(CommitOptions options)
+        {
+            _tx.Commit();
+            _db.Committed(this);
+
+            return ValueTask.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _read.Dispose();
+            // _tx doesn't have to be disposed. It should be the same as read.
+        }
+    }
+
+    private void Committed(Batch batch)
+    {
+        PushLatestReader();
+
+        var stats = _env.GetStat();
+        _depth.Set((int)stats.ms_depth);
+    }
+
+    private void PushLatestReader()
+    {
+        lock (_batchLock)
+        {
+            var reader = Batch.ReadOnly(this, _env.BeginReadOnlyTransaction());
+            _readers.Enqueue(new ReadOnlyBatchCountingRefs(reader));
+
+            // ensure memory not breached
+            if (_readers.Count > _historyDepth)
+            {
+                _readers.Dequeue().Dispose();
+            }
+        }
+    }
+
+    private interface IAccountContext
+    {
+        ReadOnlyTransaction Tx { get; }
+
+        bool CanGenerate { get; }
+
+        [Pure]
+        Span<byte> Generate(in Keccak keccak);
+    }
+
+    private readonly struct ReadonlyAccountContext(ReadOnlyTransaction tx) : IAccountContext
+    {
+        public ReadOnlyTransaction Tx { get; } = tx;
+
+        public bool CanGenerate => false;
+
+        public Span<byte> Generate(in Keccak keccak)
+        {
+            throw new Exception("Readonly");
+        }
+    }
+
+    private readonly struct ReadWriteAccountContext(Transaction tx, Db db) : IAccountContext
+    {
+        public ReadOnlyTransaction Tx => tx;
+
+        public bool CanGenerate => true;
+
+        public Span<byte> Generate(in Keccak keccak)
+        {
+            if (db.TryGet(tx, AccountCounter, out var value) == false)
+            {
+                value = new byte[] { 0 };
+            }
+
+            Span<byte> span = stackalloc byte[MaxAccountIdLength];
+            value.CopyTo(span);
+
+            var id = BinaryPrimitives.ReadUInt64LittleEndian(span) + 1;
+            BinaryPrimitives.WriteUInt64LittleEndian(span, id);
+
+            // align length to full bytes
+            var length = (64 + 7 - BitOperations.LeadingZeroCount(id)) / 8;
+
+            var sliced = span.Slice(0, length);
+
+            db.Put(tx, AccountCounter, sliced);
+            db.Put(tx, BuildAccountMapKey(keccak, stackalloc byte[AccountMapKeyLength]), sliced);
+
+            // TODO: later try to clean this up
+            return sliced.ToArray();
+        }
+    }
+}
