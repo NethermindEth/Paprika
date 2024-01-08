a26b7a7e5e412545de79ccd11a0cf451a575795b
scooletz <scooletz@gmail.com>
2024-01-08 12:01:02 +0100

    stats and others

--- src/Paprika/LMDB/Db.cs
+++ src/Paprika/LMDB/Db.cs
@@ -5,9 +5,11 @@ using System.Diagnostics.Metrics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
 using Paprika.Chain;
 using Paprika.Crypto;
 using Paprika.Data;
+using Paprika.Merkle;
 using Paprika.Store;
 using Paprika.Utils;
 using Spreads.Buffers;
@@ -49,6 +51,8 @@ public class Db : IDb, IDisposable
 
     private const int MaxAccountIdLength = 8;
     private const int AccountMapKeyLength = MetaPrefixLength + Keccak.Size;
+    private const int TypeShift = 4;
+    private const int IdLengthShift = 1;
 
     private static ReadOnlySpan<byte> MetadataKey => new byte[] { PrefixMeta, 1 };
     private static ReadOnlySpan<byte> AccountCounter => new byte[] { PrefixMeta, 2 };
@@ -123,6 +127,28 @@ public class Db : IDb, IDisposable
         }
     }
 
+    public Stats GatherStats()
+    {
+        using var read = _env.BeginReadOnlyTransaction();
+        using var cursor = _db.OpenReadOnlyCursor(read);
+
+        Stats stats = new Stats();
+
+        DirectBuffer key = default, value = default;
+        if (cursor.TryGet(ref key, ref value, CursorGetOption.First))
+        {
+            stats.Record(key, value);
+
+            while (cursor.TryGet(ref key, ref value, CursorGetOption.Next))
+            {
+                stats.Record(key, value);
+            }
+        }
+
+        return stats;
+
+    }
+
     private bool TryMapKey<TAccountContext>(in TAccountContext ctx, in Key key, Span<byte> destination,
         out Span<byte> mapped)
         where TAccountContext : struct, IAccountContext
@@ -170,9 +196,22 @@ public class Db : IDb, IDisposable
                      key.StoragePath.Length == 0 &&
                      key.Path.Length < NibblePath.KeccakNibbleCount);
 
-        // directly use path as the key. Path will have set the first bit to zero,
-        // which will not collide with the encoding above
-        mapped = key.Path.WriteTo(destination);
+
+        // shift left key by 4, so that move k up, so that the first bit is left unset
+        // store oddity at the end
+        const byte firstByte = (int)DataType.Merkle << TypeShift;
+        const int headerLength = 1;
+
+        if (key.Path.IsEmpty)
+        {
+            destination[0] = firstByte;
+            mapped = destination[..headerLength];
+            return true;
+        }
+
+        destination[0] = firstByte;
+        var written = key.Path.WriteTo(destination[headerLength..]);
+        mapped = destination[..(written.Length + headerLength)];
         return true;
     }
 
@@ -183,7 +222,7 @@ public class Db : IDb, IDisposable
         var k = key.Type | DataType.CompressedAccount;
 
         // shift left key by 4, so that move k up, so that the first bit is left unset
-        var firstByte = ((int)k << 4) + id.Length;
+        var firstByte = ((int)k << TypeShift) + (id.Length << IdLengthShift);
 
         destination[0] = (byte)firstByte;
         id.CopyTo(destination[1..]);
@@ -443,4 +482,99 @@ public class Db : IDb, IDisposable
     {
         _env.Sync(true);
     }
+
+    public class Stats
+    {
+        public int Accounts { get; private set; }
+        public long TotalSizeAccounts { get; private set; }
+        public int IdMappings { get; private set; }
+        public long TotalSizeIdMappings { get; private set; }
+
+        public long TotalSizeStorage { get; private set; }
+        public long TotalSizeMerkle { get; private set; }
+
+        public long TotalSizeEmbeddedMerkleLeafs { get; private set; }
+
+        public void Record(in DirectBuffer key, in DirectBuffer value)
+        {
+            var totalLength = key.Length + value.Length + 8;
+
+            var first = key.Span[0];
+            switch (first)
+            {
+                case PrefixAccount:
+                    Accounts++;
+                    TotalSizeAccounts += totalLength;
+                    break;
+                case PrefixMeta:
+                    {
+                        switch (key.Span.Length)
+                        {
+                            case 1 + Keccak.Size:
+                                IdMappings++;
+                                TotalSizeIdMappings += totalLength;
+                                break;
+                            case > 2:
+                                throw new Exception($"{((ReadOnlySpan<byte>)key.Span).ToHexString(true)} should not appear here");
+                        }
+
+                        break;
+                    }
+                default:
+                    {
+                        // complex key
+                        var type = (DataType)(first >> TypeShift) & ~DataType.CompressedAccount;
+
+                        switch (type)
+                        {
+                            case DataType.StorageCell:
+                                TotalSizeStorage += totalLength;
+                                break;
+                            case DataType.Merkle:
+                                TotalSizeMerkle += totalLength;
+
+                                Node.ReadFrom(value.Span, out var nodeType, out _, out _, out var branch);
+                                if (nodeType == Node.Type.Branch)
+                                {
+                                    TotalSizeEmbeddedMerkleLeafs += branch.Leafs.MaxByteSize;
+                                }
+
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                        break;
+                    }
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            var props = this.GetType().GetProperties();
+
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(this);
+
+                if (value == null)
+                {
+                    sb.AppendLine($"{prop.Name}: null");
+                }
+                else if (prop.Name.StartsWith("total", StringComparison.OrdinalIgnoreCase))
+                {
+                    var size = ((double)((long)value!)) / (1024 * 1024 * 1024);
+                    sb.AppendLine($"{prop.Name}: {size:F2}");
+                }
+                else
+                {
+                    sb.AppendLine($"{prop.Name}: {value.ToString()}");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
 }
+
