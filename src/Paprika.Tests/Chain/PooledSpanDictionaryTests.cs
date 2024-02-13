555e6c0e0350865a3afeb19dcae2cd581e0aa1cd
scooletz <scooletz@gmail.com>
2024-02-13 11:38:51 +0100

    test

---
+++ src/Paprika.Tests/Chain/PooledSpanDictionaryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Paprika.Chain;
+
+namespace Paprika.Tests.Chain;
+
+[TestFixture(true, true)]
+[TestFixture(true, false)]
+[TestFixture(false, true)]
+[TestFixture(false, false)]
+public class PooledSpanDictionaryTests(bool preserveOldValues, bool concurrentReaders)
+{
+    [Test]
+    public void Destruction()
+    {
+        using var pool = new BufferPool(4);
+        using var dict = new PooledSpanDictionary(pool, preserveOldValues, concurrentReaders);
+
+        Span<byte> key = stackalloc byte[] { 13, 17 };
+        Span<byte> value = stackalloc byte[] { 211 };
+
+        const byte metadata = 1;
+        const ulong hash = 859;
+
+        dict.Set(key, hash, value, metadata);
+        dict.TryGet(key, hash, out var actual).Should().BeTrue();
+        actual.SequenceEqual(value).Should().BeTrue();
+
+        dict.Destroy(key, hash);
+        dict.TryGet(key, hash, out var destroyed).Should().BeTrue();
+        destroyed.IsEmpty.Should().BeTrue();
+    }
+}
