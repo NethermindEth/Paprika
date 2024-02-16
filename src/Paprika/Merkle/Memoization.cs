053e4487caece63be8f0fb5eed0e0089c3dba142
Szymon Kulec <scooletz@gmail.com>
2024-02-16 18:24:30 +0100

    Memoization named (#244)

---
+++ src/Paprika/Merkle/Memoization.cs
@@ -0,0 +1,17 @@
+﻿namespace Paprika.Merkle;
+
+/// <summary>
+/// What should be memoized in Merkle as transient, to speed up the compute.
+/// </summary>
+public enum Memoization
+{
+    /// <summary>
+    /// Nothing.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// 512bytes per branch, copied over between commits. Can cost a lot in terms of memory being copied.
+    /// </summary>
+    Branch = 1,
+}
