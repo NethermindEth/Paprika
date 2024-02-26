0a47a229776674636e2558678e6e45afbd0d796f
Shubham Sonthalia <shubhamsonthalia@Shubhams-MacBook-Air.local>
2024-02-26 12:19:14 +0530

    using ArrayPool inplace of unmanaged buffer for better perf

--- src/Paprika/Utils/Xor8.cs
+++ src/Paprika/Utils/Xor8.cs
@@ -41,6 +41,7 @@ public class Xor8
         var reverseH = ArrayPool<byte>.Shared.Rent(size);
         var t2Count = ArrayPool<byte>.Shared.Rent(arrayLength);
         var t2 = ArrayPool<ulong>.Shared.Rent(arrayLength);
+        var fp = ArrayPool<byte>.Shared.Rent(arrayLength);
 
         int reverseOrderPos;
         ulong seed;
@@ -147,7 +148,6 @@ public class Xor8
 
         _seed = seed;
 
-        IntPtr fp = Marshal.AllocHGlobal(arrayLength);
         for (var i = reverseOrderPos - 1; i >= 0; i--)
         {
             var k = reverseOrder[i];
@@ -164,20 +164,19 @@ public class Xor8
                 }
                 else
                 {
-                    xor ^= Marshal.ReadByte(fp, h);
+                    xor ^= fp[h];
                 }
             }
-            Marshal.WriteByte(fp, change, (byte)xor);
+            fp[change] = (byte)xor; 
         }
-
         _fingerprints = new byte[arrayLength];
-        Marshal.Copy(fp, _fingerprints, 0, arrayLength);
+        Array.Copy(fp, _fingerprints, arrayLength);
 
         ArrayPool<ulong>.Shared.Return(reverseOrder);
         ArrayPool<byte>.Shared.Return(reverseH);
         ArrayPool<byte>.Shared.Return(t2Count);
         ArrayPool<ulong>.Shared.Return(t2);
-        Marshal.FreeHGlobal(fp);
+        ArrayPool<byte>.Shared.Return(fp);
     }
 
     public bool MayContain(ulong key)
