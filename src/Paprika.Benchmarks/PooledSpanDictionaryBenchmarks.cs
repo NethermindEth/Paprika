6dbfaa32034e9a1517d6cf72939713f1818d060c
scooletz <scooletz@gmail.com>
2024-04-03 12:55:41 +0200

    benchmarks

--- src/Paprika.Benchmarks/PooledSpanDictionaryBenchmarks.cs
+++ src/Paprika.Benchmarks/PooledSpanDictionaryBenchmarks.cs
@@ -5,11 +5,14 @@ using Paprika.Chain;
 namespace Paprika.Benchmarks;
 
 [MemoryDiagnoser]
+[DisassemblyDiagnoser]
 public class PooledSpanDictionaryBenchmarks
 {
     private readonly BufferPool _pool = new(1, false);
     private readonly PooledSpanDictionary _bigDict;
+
     private const int BigDictCount = 32_000;
+
     private static ReadOnlySpan<byte> Value32Bytes => new byte[]
     {
         0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
@@ -18,9 +21,21 @@ public class PooledSpanDictionaryBenchmarks
         0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
     };
 
+    private readonly PooledSpanDictionary _varLengthKeys;
+    private const int VarLengthKeyCollisions = 8;
+    private const ulong VarLengthKeyCollisionHash = 2348598349058394;
+
+    private static ReadOnlySpan<byte> VarLengthKey => new byte[]
+    {
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+    };
+
     public PooledSpanDictionaryBenchmarks()
     {
-        _bigDict = new PooledSpanDictionary(new BufferPool(124, false, null));
+        _bigDict = new PooledSpanDictionary(new BufferPool(128, false, null));
 
         Span<byte> key = stackalloc byte[4];
 
@@ -28,6 +43,13 @@ public class PooledSpanDictionaryBenchmarks
         {
             Unsafe.WriteUnaligned(ref key[0], i);
             _bigDict.Set(key, i, Value32Bytes, 1);
+        }
+
+        _varLengthKeys = new PooledSpanDictionary(new BufferPool(32, false, null));
+
+        for (uint i = 0; i < VarLengthKeyCollisions; i++)
+        {
+            _bigDict.Set(VarLengthKey[..VarLengthKeyCollisions], VarLengthKeyCollisionHash, Value32Bytes, 1);
         }
     }
 
@@ -44,6 +66,19 @@ public class PooledSpanDictionaryBenchmarks
             {
                 count++;
             }
+        }
+
+        return count;
+    }
+
+    [Benchmark]
+    public int Enumerate_big_dict()
+    {
+        var count = 0;
+
+        foreach (var item in _bigDict)
+        {
+            count += item.Metadata;
         }
 
         return count;
@@ -100,6 +135,20 @@ public class PooledSpanDictionaryBenchmarks
             dict.Set(key, i, key, 1);
             dict.TryGet(key, i, out var result);
             count += result[0];
+        }
+
+        return count;
+    }
+
+    [Benchmark(OperationsPerInvoke = VarLengthKeyCollisions)]
+    public int Read_colliding_hashes_different_lengths()
+    {
+        var count = 0;
+
+        for (var i = 0; i < VarLengthKeyCollisions; i++)
+        {
+            _varLengthKeys.TryGet(VarLengthKey[..(i + 1)], VarLengthKeyCollisionHash, out var data);
+            count += data.Length;
         }
 
         return count;
