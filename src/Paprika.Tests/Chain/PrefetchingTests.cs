75cf9ae1c3bf3902967bdb6ce48332611a5b7beb
scooletz <scooletz@gmail.com>
2024-04-09 18:32:52 +0200

    trying to provide a test

--- src/Paprika.Tests/Chain/PrefetchingTests.cs
+++ src/Paprika.Tests/Chain/PrefetchingTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
+using FluentAssertions;
+using Nethermind.Int256;
 using Paprika.Chain;
 using Paprika.Crypto;
 using Paprika.Merkle;
@@ -7,18 +9,73 @@ using Paprika.Store;
 
 namespace Paprika.Tests.Chain;
 
-[Explicit]
+
 public class PrefetchingTests
 {
+    [Test]
+    public async Task Prefetches_properly_on_not_changed_structure()
+    {
+        using var original = PagedDb.NativeMemoryDb(32 * 1024 * 1024, 2);
+        var db = new ReadForbiddingDb(original);
+
+        var merkle = new ComputeMerkleBehavior(ComputeMerkleBehavior.ParallelismNone);
+        await using var blockchain = new Blockchain(db, merkle);
+
+        const int blocks = 100;
+        var bigNonce = new UInt256(100, 100, 100, 100);
+
+        var random = new Random(13);
+        var accounts = new Keccak[blocks];
+        random.NextBytes(MemoryMarshal.Cast<Keccak, byte>(accounts));
+        
+        // Create structure first
+        var parent = Keccak.EmptyTreeHash;
+
+        using var start = blockchain.StartNew(parent);
+
+        for (uint account = 0; account < blocks; account++)
+        {
+            
+            start.SetAccount(accounts[account], new Account(13, bigNonce));
+        }
+
+        parent = start.Commit(1);
+        blockchain.Finalize(parent);
+        await blockchain.WaitTillFlush(1);
+
+        for (uint i = 2; i < blocks; i++)
+        {
+            var keccak = accounts[i];
+            using var block = blockchain.StartNew(parent);
+
+            db.MerkleReadsForbid(false);
+            
+            // prefetch first
+            var p = block.OpenPrefetcher();
+            p!.CanPrefetchFurther.Should().BeTrue();
+            p.PrefetchAccount(keccak);
+            
+            // forbid reads
+            db.MerkleReadsForbid(true);
+
+            block.SetAccount(keccak, new Account(i, bigNonce));
+            parent = block.Commit(i);
+            
+            blockchain.Finalize(parent);
+            await blockchain.WaitTillFlush(i);
+        }
+    }
+    
+    [Explicit]
     [TestCase(true)]
     [TestCase(false)]
-    public async Task Test(bool prefetch)
+    public async Task Spin(bool prefetch)
     {
         var commits = new Stopwatch();
 
         const int parallelism = ComputeMerkleBehavior.ParallelismNone;
         const int finalityLength = 16;
-        const int accounts = 500_000;
+        const int accounts = 1_000_000;
         const int accountsPerBlock = 50;
         const int blocks = accounts / accountsPerBlock;
 
@@ -27,7 +84,7 @@ public class PrefetchingTests
 
         random.NextBytes(MemoryMarshal.Cast<Keccak, byte>(keccaks));
 
-        using var db = PagedDb.NativeMemoryDb(512 * 1024 * 1024, 2);
+        using var db = PagedDb.NativeMemoryDb(1024 * 1024 * 1024, 2);
         var merkle = new ComputeMerkleBehavior(parallelism);
         await using var blockchain = new Blockchain(db, merkle);
         var at = 0;
---
+++ src/Paprika.Tests/ReadForbiddingDb.cs
@@ -0,0 +1,47 @@
+﻿using Paprika.Crypto;
+using Paprika.Data;
+using Paprika.Store;
+
+namespace Paprika.Tests;
+
+public class ReadForbiddingDb(IDb db) : IDb
+{
+    private bool _merkleReadsForbidden;
+    public IBatch BeginNextBatch() => db.BeginNextBatch();
+
+    public IReadOnlyBatch BeginReadOnlyBatch(string name = "") => new ReadOnlyBatch(db.BeginReadOnlyBatch(name), this);
+
+    public void Flush() => db.Flush();
+
+    public IReadOnlyBatch BeginReadOnlyBatchOrLatest(in Keccak stateHash, string name = "") =>
+        new ReadOnlyBatch(db.BeginReadOnlyBatchOrLatest(in stateHash, name), this);
+
+    public bool HasState(in Keccak keccak) => db.HasState(in keccak);
+
+    private class ReadOnlyBatch(IReadOnlyBatch batch, ReadForbiddingDb parent) : IReadOnlyBatch
+    {
+        public Metadata Metadata => batch.Metadata;
+
+        public bool TryGet(scoped in Key key, out ReadOnlySpan<byte> result)
+        {
+            parent.OnTryGet(key, this);
+            
+            return batch.TryGet(in key, out result);
+        }
+
+        public void Dispose() => batch.Dispose();
+    }
+
+    public void MerkleReadsForbid(bool forbidden)
+    {
+        _merkleReadsForbidden = forbidden;
+    }
+
+    private void OnTryGet(in Key key, IReadOnlyBatch reads)
+    {
+        if (key.Type == DataType.Merkle && _merkleReadsForbidden)
+        {
+            throw new Exception($"The key {key.ToString()} cannot be get as reads have been forbidden. At {reads.Metadata.BlockNumber}");
+        }
+    }
+}
