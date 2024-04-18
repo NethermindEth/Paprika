cd59abaf9325a8b2637c4af2a0c799b39d72ea05
Ben Adams <thundercat@illyriad.co.uk>
2024-04-18 08:31:29 +0100

    Move chonk exceptions out from inlinable

--- src/Paprika/Store/Page.cs
+++ src/Paprika/Store/Page.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Paprika.Data;
@@ -56,8 +58,14 @@ public static class PageExtensions
         {
             PageType.Leaf => new LeafPage(page).Set(key, data, batch),
             PageType.Standard => new DataPage(page).Set(key, data, batch),
-            _ => throw new ArgumentException($"Page type {page.Header.PageType} not handled")
+            _ => PageTypeNotHandled(page.Header.PageType)
         };
+
+        static Page PageTypeNotHandled(PageType pageType)
+        {
+            ThrowPageTypeNotHandled(pageType);
+            return default;
+        }
     }
 
     public static bool GetPageWithData(this Page page, IReadOnlyBatchContext batch, scoped in NibblePath key, out ReadOnlySpan<byte> result)
@@ -66,8 +74,22 @@ public static class PageExtensions
         {
             PageType.Leaf => new LeafPage(page).TryGet(batch, key, out result),
             PageType.Standard => new DataPage(page).TryGet(batch, key, out result),
-            _ => throw new ArgumentException($"Page type {page.Header.PageType} not handled")
+            _ => PageTypeNotHandled(page.Header.PageType, out result)
         };
+
+        static bool PageTypeNotHandled(PageType pageType, out ReadOnlySpan<byte> result)
+        {
+            ThrowPageTypeNotHandled(pageType);
+            result = default;
+            return false;
+        }
+    }
+
+    [DoesNotReturn]
+    [StackTraceHidden]
+    private static void ThrowPageTypeNotHandled(PageType pageType)
+    {
+        throw new ArgumentException($"Page type {pageType} not handled");
     }
 }
 
