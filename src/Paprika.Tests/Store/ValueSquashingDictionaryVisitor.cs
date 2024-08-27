70c4551e2daee4679f958577a13dc598c8d47fba
scooletz <scooletz@gmail.com>
2024-08-27 12:07:55 +0200

    dictionary

---
+++ src/Paprika.Tests/Store/ValueSquashingDictionaryVisitor.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using Paprika.Data;
+using Paprika.Store;
+
+namespace Paprika.Tests.Store;
+
+/// <summary>
+/// A visitor that allows to squash all the values in a given database to a single dictionary so that they can be expected more easly.
+/// </summary>
+/// <param name="resolver"></param>
+public class ValueSquashingDictionaryVisitor(IPageResolver resolver) : IPageVisitor
+{
+    public readonly Dictionary<byte[], byte[]> Dictionary = new();
+
+    private void ReportMap(ref NibblePath.Builder prefix, in SlottedArray map)
+    {
+        Span<byte> span = stackalloc byte[64];
+
+        foreach (var item in map.EnumerateAll())
+        {
+            var key = prefix.Append(item.Key).WriteTo(span).ToArray();
+
+            ref var value = ref CollectionsMarshal.GetValueRefOrAddDefault(Dictionary, key, out var exists);
+            if (exists == false)
+            {
+                // Set only if does not exist
+                value = item.RawData.ToArray();
+            }
+        }
+    }
+
+    public IDisposable On<TPage>(scoped ref NibblePath.Builder prefix, TPage page, DbAddress addr)
+        where TPage : unmanaged, IPage
+    {
+        var p = page.AsPage();
+        switch (p.Header.PageType)
+        {
+            case PageType.DataPage:
+                ReportMap(ref prefix, new DataPage(p).Map);
+                break;
+            case PageType.LeafOverflow:
+                ReportMap(ref prefix, new LeafOverflowPage(p).Map);
+                break;
+        }
+
+        return Disposable.Instance;
+    }
+
+    public IDisposable On<TPage>(TPage page, DbAddress addr)
+        where TPage : unmanaged, IPage =>
+        Disposable.Instance;
+
+    public IDisposable Scope(string name) => Disposable.Instance;
+}
