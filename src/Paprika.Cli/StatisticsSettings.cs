a1113147cc399c9c96d126776144086100a58a19
scooletz <scooletz@gmail.com>
2024-09-25 11:54:38 +0200

    storage assistance

---
+++ src/Paprika.Cli/StatisticsSettings.cs
@@ -0,0 +1,28 @@
+﻿using Paprika.Runner;
+using Paprika.Store;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace Paprika.Cli;
+
+public class StatisticsSettings : BasePaprikaSettings
+{
+    public class Command : Command<StatisticsSettings>
+    {
+        public override int Execute(CommandContext context, StatisticsSettings settings)
+        {
+            using var db = settings.BuildDb();
+            using var read = db.BeginReadOnlyBatch();
+
+            var stats = new Layout("Stats");
+
+            AnsiConsole.WriteLine("Gathering stats...");
+
+            StatisticsForPagedDb.Report(stats, read, db);
+
+            AnsiConsole.Write(stats);
+
+            return 0;
+        }
+    }
+}
