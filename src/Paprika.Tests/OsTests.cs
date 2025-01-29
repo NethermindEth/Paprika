feabc7cf926fa108720c3d6b7c9c175fd385bdb2
scooletz <scooletz@gmail.com>
2025-01-29 16:11:30 +0100

    multi os tests

---
+++ src/Paprika.Tests/OsTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Paprika.Tests;
+
+/// <summary>
+/// A dummy suite testing execution on different systems.
+/// </summary>
+[Category(Categories.OS)]
+public class OsTests
+{
+    [Test]
+    public void Test()
+    {
+        Console.WriteLine(RuntimeInformation.OSDescription);
+    }
+}
