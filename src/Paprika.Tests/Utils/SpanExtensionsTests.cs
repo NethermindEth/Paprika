3850b3ff1f8a373fc2fc67d459bf30887f5ec397
Szymon Kulec <scooletz@gmail.com>
2025-02-05 14:37:33 +0100

    Span consecutive batching made simpler (#471)
    * simpler batching that actually stores the value
    
    * format

--- src/Paprika.Tests/Utils/SpanExtensionsTests.cs
+++ src/Paprika.Tests/Utils/SpanExtensionsTests.cs
@@ -16,11 +16,12 @@ public class SpanExtensionsTests
     [Test]
     public void Batch_single_consecutive()
     {
-        Span<int> span = [1, 2, 3];
+        const int start0 = 1;
+        Span<int> span = [start0, 2, 3];
 
         var e = span.BatchConsecutive();
 
-        MoveNext(ref e, new(0, span.Length));
+        MoveNext(ref e, new(start0, span.Length));
 
         End(e);
     }
@@ -28,14 +29,16 @@ public class SpanExtensionsTests
     [Test]
     public void Batch_single_consecutive_with_max()
     {
-        Span<int> span = [1, 2, 3, 4];
+        const int start0 = 1;
+        const int start1 = 3;
+        Span<int> span = [start0, 2, start1, 4];
 
         const int max = 2;
 
         var e = span.BatchConsecutive(max);
 
-        MoveNext(ref e, new(0, max));
-        MoveNext(ref e, new(2, max));
+        MoveNext(ref e, new(start0, max));
+        MoveNext(ref e, new(start1, max));
 
         End(e);
     }
@@ -43,14 +46,16 @@ public class SpanExtensionsTests
     [Test]
     public void Batch_splits()
     {
-        Span<int> span = [1, 3, 4, 5, 6];
+        const int start0 = 1;
+        const int start1 = 3;
+        Span<int> span = [start0, start1, 4, 5, 6];
 
         const int at = 1;
 
         var e = span.BatchConsecutive();
 
-        MoveNext(ref e, new(0, at));
-        MoveNext(ref e, new(at, span.Length - at));
+        MoveNext(ref e, new(start0, at));
+        MoveNext(ref e, new(start1, span.Length - at));
 
         End(e);
     }
@@ -58,16 +63,21 @@ public class SpanExtensionsTests
     [Test]
     public void Batch_splits_with_max()
     {
-        Span<int> span = [1, 3, 4, 5, 6, 7];
+        const int start0 = 1;
+        const int start1 = 3;
+        const int start2 = 5;
+        const int start3 = 7;
+
+        Span<int> span = [start0, start1, 4, start2, 6, start3];
 
         const int max = 2;
 
         var e = span.BatchConsecutive(max);
 
-        MoveNext(ref e, new(0, 1));
-        MoveNext(ref e, new(1, max));
-        MoveNext(ref e, new(3, max));
-        MoveNext(ref e, new(5, 1));
+        MoveNext(ref e, new(start0, 1));
+        MoveNext(ref e, new(start1, max));
+        MoveNext(ref e, new(start2, max));
+        MoveNext(ref e, new(start3, 1));
 
         End(e);
     }
