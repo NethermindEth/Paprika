3a1e50578a9cc4a8ca35a9b67ec709f955718763
scooletz <scooletz@gmail.com>
2025-02-05 11:01:18 +0100

    amendments

---
+++ src/Paprika.OS.Tests/OsTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Paprika.Tests.OS;
+
+/// <summary>
+/// A dummy suite testing execution on different systems.
+/// </summary>
+public class OsTests
+{
+    [Test]
+    public void Test()
+    {
+        Console.WriteLine(RuntimeInformation.OSDescription);
+    }
+}
